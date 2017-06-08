46b2dde9d891bf2cf75e8c9018880a873c109c9a
Javier <jrzaurin@gmail.com>
2017-06-08 21:05:24 +0100

    Create get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-mkdir data
-cd data
+mkdir 20_newsgroup
+cd 20_newsgroup
 wget http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.tar.gz
 tar -xvf news20.tar.gz
 
