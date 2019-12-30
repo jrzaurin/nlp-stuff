bcab6b3ffba6d653c8fb137d9160b29874287a0c
jrzaurin <jrzaurin@gmail.com>
2019-12-30 18:39:01 +0100

    notebooks written. Need review and I will write the post. Also need to add comments to code

--- amazon_reviews_classification_HAN/utils/plot_attention.py
+++ amazon_reviews_classification_HAN/utils/plot_attention.py
@@ -1,16 +1,33 @@
+"""
+Code here based on: https://gist.github.com/ihsgnef/f13c35cd46624c8f458a4d23589ac768
+"""
+
 import matplotlib
 
 
-def attention_to_html(text, attnw, out_path, cmap="Greens"):
+def plot_word_attention(doc, doc_w, cmap="Greens"):
+
     cmap = matplotlib.cm.get_cmap(cmap)
-    html_chars = []
-    for t, w in zip(text, attnw):
-        r, g, b, a = cmap(w)
-        r, g, b = int(256 * r), int(256 * g), int(256 * b)
-        t = '<font face="monospace" \nsize="3">%s</font>' % t
-        html_chars.append(
-            '<span style="background-color:rgb(%s, %s, %s); color:black;">%s</span>' % (r, g, b, t)
-        )
-    tot_html = " ".join(html_chars)
-    with open(out_path, "w") as out:
-        out.write(tot_html)
+    template = '<font face="monospace" \nsize="3"; span class="barcode"; style="color: black; background-color: {}">{}</span>'
+    colored_doc = ""
+
+    for sent, sent_w in zip(doc, doc_w):
+        for t, w in zip(sent.split(), sent_w):
+            color = matplotlib.colors.rgb2hex(cmap(w)[:3])
+            colored_doc += template.format(color, "&nbsp" + t + "&nbsp")
+        colored_doc += "</br>"
+
+    return colored_doc
+
+
+def plot_sent_attention(doc, doc_w, cmap="Greens"):
+
+    cmap = matplotlib.cm.get_cmap(cmap)
+    template = '<font face="monospace" \nsize="3"; span class="barcode"; style="color: black; background-color: {}">{}</span>'
+    colored_doc = ""
+
+    for sent, sent_w in zip(doc, doc_w):
+        color = matplotlib.colors.rgb2hex(cmap(sent_w)[:3])
+        colored_doc += template.format(color, "&nbsp" + sent + "&nbsp") + "</br>"
+
+    return colored_doc
